73d6742cbf3741536b45d8393020441bac5b5824
Harsh Anand <developersharsh25@gmail.com>
2023-07-31 20:32:43 +0530

    questions_summary.dart file created

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/data/questions.dart';
+import 'package:quiz_app_flutter/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
 
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -15,17 +39,18 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have answered X out of Y answers correctly'),
+            Text(
+                'You have answered $numCorrectQuestions out of $numTotalQuestions answers correctly'),
             const SizedBox(
               height: 30,
             ),
-            const Text('List of answeres and questions....'),
+            QuestionSummary(summaryData),
             const SizedBox(
               height: 30,
             ),
             TextButton(
               onPressed: () {},
-              child: Text('Restart Quiz'),
+              child: const Text('Restart Quiz'),
             ),
           ],
         ),
